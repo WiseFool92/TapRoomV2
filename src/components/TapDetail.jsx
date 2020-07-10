0c95247f1190e6605237967cb83ff230c3568e6d
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-10 15:38:33 -0700

    update detail display

--- src/components/TapDetail.jsx
+++ src/components/TapDetail.jsx
@@ -7,12 +7,16 @@ function TapDetail(props) {
   const { tap } = props;
   return (
     <>
-      <h2>Tap Details</h2><br />
-      <h3>{tap.name}</h3><br />
-      <h4>Brand: {tap.brand}</h4><br />
-      <p>Price: {tap.price}</p><br />
-      <p>Alcohol Content: {tap.alcoholContent}</p><br />
-      <p>Pints Left: {tap.pints}</p><hr />
+    <div class="display">
+      <Button color="info">
+        <h2>Tap Details</h2><br />
+        <h3>{tap.name}</h3><br />
+        <h4>Brand: {tap.brand}</h4><br />
+        <p>Price: {tap.price}</p><br />
+        <p>Alcohol Content: {tap.alcoholContent}</p><br />
+        <p>Pints Left: {tap.pints}</p><hr />
+      </Button>
+    </div>
       <div class = "form">
         <Button color = "primary" class = "btn" onClick = { props.onClickingEdit }>Update</Button>
         <Button color = "danger" class = "btn" onClick = {() => props.onClickingDelete(tap.id) }>Delete</Button>
