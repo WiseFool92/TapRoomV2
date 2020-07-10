9b0c4aaf5a83cd74af9ec5878100607ed3347f54
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-10 09:25:00 -0700

    populate ReusableForm

---
+++ src/components/ReusableForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+
+function ReusableForm(props) {
+  return (
+    <>
+      <form onSubmit = {props.formSubmissionHandler}>
+        <input
+          type = 'text'
+          name = 'name'
+          placeholder = 'Tap Name' /><br /><hr />
+        <input
+          type = 'text'
+          name = 'brand'
+          placeholder = 'Tap Brand' /><br /><hr />
+        <input
+          type = 'number'
+          name = 'price'
+          placeholder = 'Tap Price' /><br /><hr />
+        <input
+          type = 'text'
+          name = 'alcoholContent'
+          placeholder = 'Proof' /><br /><hr />
+        <input
+          type = 'number'
+          name = 'pints'
+          placeholder = 'Number of Pints available' /><br /><hr />
+        <Button type = 'submit'>{props.buttonText}</Button>
+      </form>
+    </>
+  )
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
