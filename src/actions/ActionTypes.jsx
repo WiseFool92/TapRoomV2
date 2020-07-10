24705d245eb784e97ecd97d8851bb80fe9b71987
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-10 09:11:37 -0700

    Add Actions

---
+++ src/actions/ActionTypes.jsx
@@ -0,0 +1,5 @@
+export const ADD_TICKET = 'ADD_TAP';
+
+export const DELETE_TICKET = 'DELETE_TAP';
+
+export const TOGGLE_FORM = 'TOGGLE_FORM';
---
+++ src/actions/index.jsx
@@ -0,0 +1,23 @@
+import * as c from './../actions/ActionTypes';
+
+export const deleteTap = id => ({
+  type: c.DELETE_TAP,
+  id
+});
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
+
+export const addTap = (tap) => {
+  const { name, brand, price, alcoholContent, pints, id } = tap;
+  return {
+    type: c.ADD_TAP,
+    name: name,
+    brand: brand,
+    price: price,
+    alcoholContent: alcoholContent,
+    pints: pints,
+    id: id
+  }
+}
